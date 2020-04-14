7cb3fb694a57b76746dd0af7b51820a6b64c89d6
Darantir <yakushev_1996@inbox.ru>
2020-04-14 21:06:43 +0200

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { DemoSkuFormWithBuilderComponent } from './demo-sku-form-with-builder/de
 import { DemoSkuFormWithValidationComponent } from './demo-sku-form-with-validation/demo-sku-form-with-validation.component';
 import { DemoSkuFormWithCustomValidationComponent } from './demo-sku-form-with-custom-validation/demo-sku-form-with-custom-validation.component';
 import { DemoFormWithEventsComponent } from './demo-form-with-events/demo-form-with-events.component';
+import { DemoFormNgModelComponent } from './demo-form-ng-model/demo-form-ng-model.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, HelloComponent, DemoSkuFormComponent, DemoSkuFormWithBuilderComponent, DemoSkuFormWithValidationComponent, DemoSkuFormWithCustomValidationComponent, DemoFormWithEventsComponent ],
+  declarations: [ AppComponent, HelloComponent, DemoSkuFormComponent, DemoSkuFormWithBuilderComponent, DemoSkuFormWithValidationComponent, DemoSkuFormWithCustomValidationComponent, DemoFormWithEventsComponent, DemoFormNgModelComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
